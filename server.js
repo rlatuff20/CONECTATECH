e5558640bcd421c49d6375a0c6e076721164ed55
rlatuff20 <rlatuff20@gmail.com>
2026-05-19 18:23:52 -0400

    Add CORS and JSON middleware to server
    Added middleware for CORS and JSON parsing.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const path = require('path');
 const app= express();
 
+app.use(express.json());
+app.use(cors());
 app.use(express.static(path.join(__dirname)));
 
 app.use(cors());
