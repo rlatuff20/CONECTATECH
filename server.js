8f954a04e63529bd9ded6cb131cc0998d3bacfdf
rlatuff20 <rlatuff20@gmail.com>
2026-05-19 17:58:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const app= express();
+
 app.use(express.static(path.join(__dirname)));
 
 app.use(cors());
